c634cd3ac942e216ad2a94df8c5fe91d6d54aa87
CrudaLilium <holec.mh@gmail.com>
2021-05-16 21:36:29 +0200

    added generating player pieces and rendering them

--- src/Game.ts
+++ src/Game.ts
@@ -4,117 +4,134 @@ export interface IBoardCell {
     y: number;
 }
 
-export interface IPlayerCell {
-    playerlink: number;
+export interface IPlayer {
+    name: string;
     color: string;
 }
 
+export interface IPlayerCell {
+    player: IPlayer;
+}
+
+export interface IPlayerPiece {
+    position: IBoardCell;
+    player: IPlayer;
+    code: string;
+}
+
 export class Game {
+    public players: IPlayer[];
     public board: IBoardCell[];
-    public playerCells : Array<IBoardCell & IPlayerCell>;
+    public playerCells: Array<IBoardCell & IPlayerCell>;
+    public playerPieces: IPlayerPiece[];
 
     constructor() {
-        this.board = this.getBoardInfo();
-        this.playerCells = this.getPlayerBoardCells();
+        this.generatePlayers();
+        this.generateBoardInfo();
+        this.generatePlayerBoardCells();
+        this.generatePlayerPiecesAtStart();
     }
 
-    private getBoardInfo(): IBoardCell[] {
-        const board: IBoardCell[] = [];
-        board.push({ index: 0, x: 0, y: 4 });
-        board.push({ index: 1, x: 1, y: 4 });
-        board.push({ index: 2, x: 2, y: 4 });
-        board.push({ index: 3, x: 3, y: 4 });
-        board.push({ index: 4, x: 4, y: 4 });
-        board.push({ index: 5, x: 4, y: 3 });
-        board.push({ index: 6, x: 4, y: 2 });
-        board.push({ index: 7, x: 4, y: 1 });
-        board.push({ index: 8, x: 4, y: 0 });
-        board.push({ index: 9, x: 5, y: 0 });
-    
-        board.push({ index: 10, x: 6, y: 0 });
-        board.push({ index: 11, x: 6, y: 1 });
-        board.push({ index: 12, x: 6, y: 2 });
-        board.push({ index: 13, x: 6, y: 3 });
-        board.push({ index: 14, x: 6, y: 4 });
-        board.push({ index: 15, x: 7, y: 4 });
-        board.push({ index: 16, x: 8, y: 4 });
-        board.push({ index: 17, x: 9, y: 4 });
-        board.push({ index: 18, x: 10, y: 4 });
-        board.push({ index: 19, x: 10, y: 5 });
-    
-        board.push({ index: 20, x: 10, y: 6 });
-        board.push({ index: 21, x: 9, y: 6 });
-        board.push({ index: 22, x: 8, y: 6 });
-        board.push({ index: 23, x: 7, y: 6 });
-        board.push({ index: 24, x: 6, y: 6 });
-        board.push({ index: 25, x: 6, y: 7 });
-        board.push({ index: 26, x: 6, y: 8 });
-        board.push({ index: 27, x: 6, y: 9 });
-        board.push({ index: 28, x: 6, y: 10 });
-        board.push({ index: 29, x: 5, y: 10 });
-    
-        board.push({ index: 30, x: 4, y: 10 });
-        board.push({ index: 31, x: 4, y: 9 });
-        board.push({ index: 32, x: 4, y: 8 });
-        board.push({ index: 33, x: 4, y: 7 });
-        board.push({ index: 34, x: 4, y: 6 });
-        board.push({ index: 35, x: 3, y: 6 });
-        board.push({ index: 36, x: 2, y: 6 });
-        board.push({ index: 37, x: 1, y: 6 });
-        board.push({ index: 38, x: 0, y: 6 });
-        board.push({ index: 39, x: 0, y: 5 });
-    
-        return board;
+    private generatePlayerPiecesAtStart(): void {
+        this.playerPieces = [];
+        for (let p = 0; p < this.players.length; p++) {
+            const playerInfo = this.players[p];
+            for (let i = 0; i < 4; i++) {
+                const position = [...this.board, ...this.playerCells].find(x => x.index == 200 + (p * 10) + i)
+                this.playerPieces.push({ code: i.toString(), player: playerInfo, position: position });
+            }
+        }
     }
-    
-    private getPlayerBoardCells(): Array<IBoardCell & IPlayerCell> {
-        const cells: Array<IBoardCell & IPlayerCell> = [];
-    
-        const player1Info = { color: "red", playerlink: 0 };
-        cells.push({ index: 0, x: 0, y: 4, ...player1Info });
-        cells.push({ index: 100, x: 1, y: 5, ...player1Info });
-        cells.push({ index: 101, x: 2, y: 5, ...player1Info });
-        cells.push({ index: 102, x: 3, y: 5, ...player1Info });
-        cells.push({ index: 102, x: 4, y: 5, ...player1Info });
-        cells.push({ index: 200, x: 0, y: 0, ...player1Info });
-        cells.push({ index: 201, x: 0, y: 1, ...player1Info });
-        cells.push({ index: 202, x: 1, y: 0, ...player1Info });
-        cells.push({ index: 203, x: 1, y: 1, ...player1Info });
-    
-        const player2Info = { color: "green", playerlink: 1 };
-        cells.push({ index: 10, x: 6, y: 0, ...player2Info });
-        cells.push({ index: 110, x: 5, y: 1, ...player2Info });
-        cells.push({ index: 111, x: 5, y: 2, ...player2Info });
-        cells.push({ index: 112, x: 5, y: 3, ...player2Info });
-        cells.push({ index: 112, x: 5, y: 4, ...player2Info });
-        cells.push({ index: 210, x: 9, y: 0, ...player2Info });
-        cells.push({ index: 211, x: 10, y: 0, ...player2Info });
-        cells.push({ index: 212, x: 9, y: 1, ...player2Info });
-        cells.push({ index: 213, x: 10, y: 1, ...player2Info });
-    
-        const player3Info = { color: "#00aaff", playerlink: 2 };
-        cells.push({ index: 20, x: 10, y: 6, ...player3Info });
-        cells.push({ index: 120, x: 9, y: 5, ...player3Info });
-        cells.push({ index: 121, x: 8, y: 5, ...player3Info });
-        cells.push({ index: 122, x: 7, y: 5, ...player3Info });
-        cells.push({ index: 122, x: 6, y: 5, ...player3Info });
-        cells.push({ index: 220, x: 9, y: 9, ...player3Info });
-        cells.push({ index: 221, x: 10, y: 9, ...player3Info });
-        cells.push({ index: 222, x: 9, y: 10, ...player3Info });
-        cells.push({ index: 223, x: 10, y: 10, ...player3Info });
-    
-        const player4Info = { color: "yellow", playerlink: 3 };
-        cells.push({ index: 30, x: 4, y: 10, ...player4Info });
-        cells.push({ index: 130, x: 5, y: 9, ...player4Info });
-        cells.push({ index: 131, x: 5, y: 8, ...player4Info });
-        cells.push({ index: 132, x: 5, y: 7, ...player4Info });
-        cells.push({ index: 132, x: 5, y: 6, ...player4Info });
-    
-        cells.push({ index: 230, x: 0, y: 9, ...player4Info });
-        cells.push({ index: 231, x: 1, y: 9, ...player4Info });
-        cells.push({ index: 232, x: 0, y: 10, ...player4Info });
-        cells.push({ index: 233, x: 1, y: 10, ...player4Info });
-    
-        return cells;
+
+    private generatePlayers(): void {
+        this.players = [];
+        this.players.push({ color: "red", name: "Player 1" });
+        this.players.push({ color: "green", name: "Player 2" });
+        this.players.push({ color: "#00aaff", name: "Player 3" });
+        this.players.push({ color: "yellow", name: "Player 4" });
+    }
+
+    private generateBoardInfo(): void {
+        this.board = [];
+        this.board.push({ index: 0, x: 0, y: 4 });
+        this.board.push({ index: 1, x: 1, y: 4 });
+        this.board.push({ index: 2, x: 2, y: 4 });
+        this.board.push({ index: 3, x: 3, y: 4 });
+        this.board.push({ index: 4, x: 4, y: 4 });
+        this.board.push({ index: 5, x: 4, y: 3 });
+        this.board.push({ index: 6, x: 4, y: 2 });
+        this.board.push({ index: 7, x: 4, y: 1 });
+        this.board.push({ index: 8, x: 4, y: 0 });
+        this.board.push({ index: 9, x: 5, y: 0 });
+        this.board.push({ index: 10, x: 6, y: 0 });
+        this.board.push({ index: 11, x: 6, y: 1 });
+        this.board.push({ index: 12, x: 6, y: 2 });
+        this.board.push({ index: 13, x: 6, y: 3 });
+        this.board.push({ index: 14, x: 6, y: 4 });
+        this.board.push({ index: 15, x: 7, y: 4 });
+        this.board.push({ index: 16, x: 8, y: 4 });
+        this.board.push({ index: 17, x: 9, y: 4 });
+        this.board.push({ index: 18, x: 10, y: 4 });
+        this.board.push({ index: 19, x: 10, y: 5 });
+        this.board.push({ index: 20, x: 10, y: 6 });
+        this.board.push({ index: 21, x: 9, y: 6 });
+        this.board.push({ index: 22, x: 8, y: 6 });
+        this.board.push({ index: 23, x: 7, y: 6 });
+        this.board.push({ index: 24, x: 6, y: 6 });
+        this.board.push({ index: 25, x: 6, y: 7 });
+        this.board.push({ index: 26, x: 6, y: 8 });
+        this.board.push({ index: 27, x: 6, y: 9 });
+        this.board.push({ index: 28, x: 6, y: 10 });
+        this.board.push({ index: 29, x: 5, y: 10 });
+        this.board.push({ index: 30, x: 4, y: 10 });
+        this.board.push({ index: 31, x: 4, y: 9 });
+        this.board.push({ index: 32, x: 4, y: 8 });
+        this.board.push({ index: 33, x: 4, y: 7 });
+        this.board.push({ index: 34, x: 4, y: 6 });
+        this.board.push({ index: 35, x: 3, y: 6 });
+        this.board.push({ index: 36, x: 2, y: 6 });
+        this.board.push({ index: 37, x: 1, y: 6 });
+        this.board.push({ index: 38, x: 0, y: 6 });
+        this.board.push({ index: 39, x: 0, y: 5 });
+    }
+
+    private generatePlayerBoardCells(): void {
+        this.playerCells = [];
+        this.playerCells.push({ index: 0, x: 0, y: 4, player: this.players[0] });
+        this.playerCells.push({ index: 100, x: 1, y: 5, player: this.players[0] });
+        this.playerCells.push({ index: 101, x: 2, y: 5, player: this.players[0] });
+        this.playerCells.push({ index: 102, x: 3, y: 5, player: this.players[0] });
+        this.playerCells.push({ index: 102, x: 4, y: 5, player: this.players[0] });
+        this.playerCells.push({ index: 200, x: 0, y: 0, player: this.players[0] });
+        this.playerCells.push({ index: 201, x: 0, y: 1, player: this.players[0] });
+        this.playerCells.push({ index: 202, x: 1, y: 0, player: this.players[0] });
+        this.playerCells.push({ index: 203, x: 1, y: 1, player: this.players[0] });
+        this.playerCells.push({ index: 10, x: 6, y: 0, player: this.players[1] });
+        this.playerCells.push({ index: 110, x: 5, y: 1, player: this.players[1] });
+        this.playerCells.push({ index: 111, x: 5, y: 2, player: this.players[1] });
+        this.playerCells.push({ index: 112, x: 5, y: 3, player: this.players[1] });
+        this.playerCells.push({ index: 112, x: 5, y: 4, player: this.players[1] });
+        this.playerCells.push({ index: 210, x: 9, y: 0, player: this.players[1] });
+        this.playerCells.push({ index: 211, x: 10, y: 0, player: this.players[1] });
+        this.playerCells.push({ index: 212, x: 9, y: 1, player: this.players[1] });
+        this.playerCells.push({ index: 213, x: 10, y: 1, player: this.players[1] });
+        this.playerCells.push({ index: 20, x: 10, y: 6, player: this.players[2] });
+        this.playerCells.push({ index: 120, x: 9, y: 5, player: this.players[2] });
+        this.playerCells.push({ index: 121, x: 8, y: 5, player: this.players[2] });
+        this.playerCells.push({ index: 122, x: 7, y: 5, player: this.players[2] });
+        this.playerCells.push({ index: 122, x: 6, y: 5, player: this.players[2] });
+        this.playerCells.push({ index: 220, x: 9, y: 9, player: this.players[2] });
+        this.playerCells.push({ index: 221, x: 10, y: 9, player: this.players[2] });
+        this.playerCells.push({ index: 222, x: 9, y: 10, player: this.players[2] });
+        this.playerCells.push({ index: 223, x: 10, y: 10, player: this.players[2] });
+        this.playerCells.push({ index: 30, x: 4, y: 10, player: this.players[3] });
+        this.playerCells.push({ index: 130, x: 5, y: 9, player: this.players[3] });
+        this.playerCells.push({ index: 131, x: 5, y: 8, player: this.players[3] });
+        this.playerCells.push({ index: 132, x: 5, y: 7, player: this.players[3] });
+        this.playerCells.push({ index: 132, x: 5, y: 6, player: this.players[3] });
+        this.playerCells.push({ index: 230, x: 0, y: 9, player: this.players[3] });
+        this.playerCells.push({ index: 231, x: 1, y: 9, player: this.players[3] });
+        this.playerCells.push({ index: 232, x: 0, y: 10, player: this.players[3] });
+        this.playerCells.push({ index: 233, x: 1, y: 10, player: this.players[3] });
     }
 }
--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { RefObject } from 'react';
 import { App } from './App';
+import { IPlayerCell } from './Game';
 
 export interface IMainProps {
     app: App;
@@ -9,26 +10,28 @@ export interface IMainProps {
 export class Main extends React.Component<IMainProps, {}>
 {
     private canvas: RefObject<HTMLCanvasElement | null>;
+    private canvas2: RefObject<HTMLCanvasElement | null>;
     constructor(props: IMainProps) {
         super(props);
         this.canvas = React.createRef<HTMLCanvasElement | null>();
+        this.canvas2 = React.createRef<HTMLCanvasElement | null>();
     }
 
     componentDidMount() {
         this.drawBoard();
+        this.drawPlayers();
     }
 
-    drawBoard() {
+    private drawBoard() {
         const cellSize = 48;
         const canvasEl = this.canvas.current as HTMLCanvasElement;
         const ctx = canvasEl.getContext("2d");
-        this.props.app.game
         const boardInfo = this.props.app.game.board;
         const playerCellsInfo = this.props.app.game.playerCells;
         const margin = 10;
 
         for (let cell of [...boardInfo, ...playerCellsInfo]) {
-            ctx.fillStyle = ('color' in cell && cell['color']) || "#EBEBEB";
+            ctx.fillStyle = ('player' in cell && (cell as IPlayerCell).player.color) || "#EBEBEB";
             ctx.fillRect(margin + cell.x * cellSize, margin + cell.y * cellSize, cellSize, cellSize);
         }
         for (let cell of [...boardInfo, ...playerCellsInfo]) {
@@ -44,11 +47,36 @@ export class Main extends React.Component<IMainProps, {}>
         }
     }
 
+    private drawPlayers() {
+        const cellSize = 48;
+        const margin = 10;
+        const canvasEl = this.canvas2.current as HTMLCanvasElement;
+        const ctx = canvasEl.getContext("2d");
+        const playerPieces = this.props.app.game.playerPieces;
+
+        for (let playerPiece of playerPieces) {
+            const boardCell = playerPiece.position;
+            const centerX = margin + boardCell.x * cellSize + cellSize / 2;
+            const centerY = margin + boardCell.y * cellSize + cellSize / 2;
+            ctx.strokeStyle = "black";
+            ctx.beginPath();
+            ctx.arc(centerX, centerY, cellSize / 2 - 5, 0, Math.PI * 2);
+            ctx.arc(centerX, centerY, cellSize / 2 - 15, 0, Math.PI * 2);
+            ctx.stroke();
+            ctx.beginPath();
+            ctx.arc(centerX, centerY, 4, 0, Math.PI * 2);
+            ctx.fill();
+        }
+    }
+
     public render(): JSX.Element {
         return (
             <>
                 <div>Hello from Main.tsx</div>
-                <canvas width="550" height="550" ref={this.canvas} ></canvas>
+                <div style={{ position: "relative" }}>
+                    <canvas width="550" height="550" style={{ position: "absolute" }} ref={this.canvas}></canvas>
+                    <canvas width="550" height="550" style={{ position: "absolute" }} ref={this.canvas2}></canvas>
+                </div>
             </>
         );
     }
