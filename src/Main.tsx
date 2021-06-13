2235e139fbac1620aaaba4904c80b3350baa2e42
CrudaLilium <holec.mh@gmail.com>
2021-06-13 19:55:22 +0200

    changed player pieces to pawns, added highlighting

--- src/Main.tsx
+++ src/Main.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { RefObject } from 'react';
 import { App } from './App';
+import { ETurnState } from './Game';
 import { IPlayerCell } from './GameStructs';
 
 export interface IMainProps {
@@ -12,11 +13,13 @@ export class Main extends React.Component<IMainProps, { lastRoll?: number }>
     private canvas: RefObject<HTMLCanvasElement | null>;
     private canvas2: RefObject<HTMLCanvasElement | null>;
     private canvas3: RefObject<HTMLCanvasElement | null>;
+    private canvas4: RefObject<HTMLCanvasElement | null>;
     constructor(props: IMainProps) {
         super(props);
         this.canvas = React.createRef<HTMLCanvasElement | null>();
         this.canvas2 = React.createRef<HTMLCanvasElement | null>();
         this.canvas3 = React.createRef<HTMLCanvasElement | null>();
+        this.canvas4 = React.createRef<HTMLCanvasElement | null>();
         this.roll = this.roll.bind(this);
         this.onHit = this.onHit.bind(this);
         this.state = {};
@@ -24,12 +27,50 @@ export class Main extends React.Component<IMainProps, { lastRoll?: number }>
 
     componentDidMount() {
         this.drawBoards();
+        this.startHighlighting();
     }
 
     private drawBoards() {
         this.drawBoard();
         this.drawPlayers();
         this.drawHitDetection();
+    }
+
+    private startHighlighting() {
+        const canvasEl = this.canvas4.current as HTMLCanvasElement;
+        const ctx = canvasEl.getContext("2d", { alpha: true });
+        ctx.clearRect(0, 0, 550, 550);
+        const red = Math.floor(new Date().getTime() / 10 % 255);
+        const green = Math.floor(new Date().getTime() / 100 % 255);
+        const blue = Math.floor(new Date().getTime() / 1000 % 255);
+        ctx.fillStyle = `rgba(${red}, ${green}, ${blue})`;
+        ctx.fillRect(10 + 2 + 5 * 48, 10 + 2 + 5 * 48, 44, 44);
+
+        if(this.props.app.game.turn.state === ETurnState.waitingForPieceSelection){
+            // highlight player selectable pieces;
+            const cellSize = 48;
+            const cellMargin = 4;
+            const margin = 10;
+            const pawnPath = new Path2D("m 20 100 q 0 -40 20 -60 a -20 -20 0 1 1 20 0 q 20 20 20 60 z ");
+            ctx.strokeStyle = "black";
+            ctx.lineWidth = 1;
+            ctx.shadowColor = "yellow";
+            ctx.shadowBlur = 6;
+            const playerPieces = this.props.app.game.playerPieces.filter(piece => piece.player === this.props.app.game.turn.currentPlayer);
+            for (let playerPiece of playerPieces) {
+                const boardCell = playerPiece.position;
+                const matrix2: DOMMatrix2DInit = {
+                    a: (cellSize - cellMargin * 2) / 100, b: 0, c: 0,
+                    d: (cellSize - cellMargin * 2) / 100, e: margin + boardCell.x * cellSize + cellMargin, f: margin + boardCell.y * cellSize + cellMargin
+                };
+                const pawn = new Path2D();
+                pawn.addPath(pawnPath, matrix2);
+                ctx.fillStyle = playerPiece.player.color;
+                ctx.fill(pawn);
+                ctx.stroke(pawn);
+            }
+        }
+        requestAnimationFrame(this.startHighlighting.bind(this));
     }
 
     private drawBoard() {
@@ -59,26 +100,28 @@ export class Main extends React.Component<IMainProps, { lastRoll?: number }>
 
     private drawPlayers() {
         const cellSize = 48;
+        const cellMargin = 4;
         const margin = 10;
         const canvasEl = this.canvas2.current as HTMLCanvasElement;
         const ctx = canvasEl.getContext("2d");
-
         ctx.clearRect(0, 0, canvasEl.width, canvasEl.height);
-
+        const pawnPath = new Path2D("m 20 100 q 0 -40 20 -60 a -20 -20 0 1 1 20 0 q 20 20 20 60 z ");
+        ctx.strokeStyle = "black";
+        ctx.lineWidth = 1;
+        ctx.shadowColor = "black";
+        ctx.shadowBlur = 3;
         const playerPieces = this.props.app.game.playerPieces;
-
         for (let playerPiece of playerPieces) {
             const boardCell = playerPiece.position;
-            const centerX = margin + boardCell.x * cellSize + cellSize / 2;
-            const centerY = margin + boardCell.y * cellSize + cellSize / 2;
-            ctx.strokeStyle = playerPiece.player.color;
-            ctx.beginPath();
-            ctx.arc(centerX, centerY, cellSize / 2 - 5, 0, Math.PI * 2);
-            ctx.arc(centerX, centerY, cellSize / 2 - 15, 0, Math.PI * 2);
-            ctx.stroke();
-            ctx.beginPath();
-            ctx.arc(centerX, centerY, 4, 0, Math.PI * 2);
-            ctx.fill();
+            const matrix2: DOMMatrix2DInit = {
+                a: (cellSize - cellMargin * 2) / 100, b: 0, c: 0,
+                d: (cellSize - cellMargin * 2) / 100, e: margin + boardCell.x * cellSize + cellMargin, f: margin + boardCell.y * cellSize + cellMargin
+            };
+            const pawn = new Path2D();
+            pawn.addPath(pawnPath, matrix2);
+            ctx.fillStyle = playerPiece.player.color;
+            ctx.fill(pawn);
+            ctx.stroke(pawn);
         }
     }
 
@@ -108,12 +151,9 @@ export class Main extends React.Component<IMainProps, { lastRoll?: number }>
         const rect = canvasEl.getBoundingClientRect();
         const x = event.clientX - rect.left;
         const y = event.clientY - rect.top;
-        //console.log("x,y", x, y);
         const ctx = canvasEl.getContext("2d");
         const pixel = ctx.getImageData(x, y, 1, 1).data;
-        //console.log(pixel);
         this.props.app.game.pieceSelect(pixel[0]);
-        
         this.drawPlayers();
         this.drawHitDetection();
     }
@@ -145,6 +185,7 @@ export class Main extends React.Component<IMainProps, { lastRoll?: number }>
                     <canvas id="hit-detection-layer" width="550" height="550" style={{ position: "absolute" }} ref={this.canvas3} onClick={this.onHit}></canvas>
                     <canvas id="background-layer" width="550" height="550" style={{ position: "absolute", pointerEvents: "none" }} ref={this.canvas}></canvas>
                     <canvas id="pieces-layer" width="550" height="550" style={{ position: "absolute", pointerEvents: "none" }} ref={this.canvas2}></canvas>
+                    <canvas id="highligh-layer" width="550" height="550" style={{ position: "absolute", pointerEvents: "none" }} ref={this.canvas4}></canvas>
                 </div>
             </>
         );
